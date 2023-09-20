e4e543164d0f211d30bb56338aed672851cdce42
ldlaigo <119918582+ldlaigo@users.noreply.github.com>
2023-09-20 19:31:09 +0800

    Initial Commit

---
+++ MyWebApplication1/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace MyWebApplication1.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
